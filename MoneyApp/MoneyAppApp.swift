39cb802798801a1d762712c144a27477e4ac495a
Grigory Sapogov <grisha.sapgv@mail.ru>
2024-04-02 12:09:45 +0500

    Initial Commit

---
+++ MoneyApp/MoneyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyAppApp.swift
+//  MoneyApp
+//
+//  Created by Grigory Sapogov on 02.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoneyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
