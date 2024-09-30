8232437e2be9a772043a0eb45273971151b3136d
Grigory Sapogov <grisha.sapgv@mail.ru>
2024-09-30 17:02:18 +0500

    Убрал долги и контакты.

--- MoneyApp/Views/MainView/MainView.swift
+++ MoneyApp/Views/MainView/MainView.swift
@@ -46,13 +46,13 @@ struct MainView: View {
                         Text(ObjectType.catalogStateConsumption.listTitle)
                     }
                     
-                    NavigationLink(value: Coordinator.MainView.catalogContact) {
-                        Text(ObjectType.catalogContact.listTitle)
-                    }
+//                    NavigationLink(value: Coordinator.MainView.catalogContact) {
+//                        Text(ObjectType.catalogContact.listTitle)
+//                    }
                     
-                    NavigationLink(value: Coordinator.MainView.catalogDebt) {
-                        Text(ObjectType.catalogDebt.listTitle)
-                    }
+//                    NavigationLink(value: Coordinator.MainView.catalogDebt) {
+//                        Text(ObjectType.catalogDebt.listTitle)
+//                    }
                     
                     NavigationLink(value: Coordinator.MainView.catalogWallet) {
                         Text(ObjectType.catalogWallet.listTitle)
